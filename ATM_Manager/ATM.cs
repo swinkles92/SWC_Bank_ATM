2d2735b38552337c5edeb8f13af7bc3af89d54ab
swinkles92 <swinkles92@live.com>
2023-05-16 15:02:29 -0600

    First dev branch commit
    Created ATM, ATMHub classes. Added and performed unit testing for ATMHub method "startHub".

---
+++ ATM_Manager/ATM.cs
@@ -0,0 +1,39 @@
+﻿namespace SWCBank;
+
+public class ATM
+{
+    public int id_Num { get; set; }
+    public decimal cash_On_Hand { get; set; }
+    public string? location { get; set; }
+
+    public void DisplayOptions()
+    {
+        WriteLine("Welcome to SWC Bank!");
+        WriteLine("Please enter the number of your desired " +
+            "transaction from the list below:");
+        WriteLine("(1) Deposit cash");
+        WriteLine("(2) Withdraw cash");
+        WriteLine("(0) End session");
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ATM"/> class.
+    /// </summary>
+    /// <param name="id_Num">Identification number of the ATM</param>
+    /// <param name="cash_On_Hand">Amount of cash (dollars) currently held
+    /// inside the ATM</param>
+    /// <param name="location">Physical location (city) of the ATM</param>
+    public ATM(int id_Num, decimal cash_On_Hand, string location)
+    {
+        this.id_Num = id_Num;
+        this.cash_On_Hand = cash_On_Hand;
+        this.location = location;
+    }
+
+    /// <summary>
+    /// Default initialization for an instance of the <see cref="ATM"/> class.
+    /// </summary>
+    public ATM()
+    {
+    }
+}
--- ATM_Manager/Program.cs
+++ ATM_Manager/Program.cs
@@ -1,3 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using SWCBank;
 
+ATM_Hub hub = new();
+hub.startHub();
+
+WriteLine("Enter the number of the desired ATM location from the list below:");
+foreach(ATM atm in hub.connected_ATMs)
+{
+    WriteLine($"{atm.id_Num}: {atm.location}");
+}
+int userLocationSelection;
+int.TryParse(ReadLine(), out userLocationSelection);
+WriteLine($"You have selected {userLocationSelection}");
