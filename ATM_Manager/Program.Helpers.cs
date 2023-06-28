a71bad1f315a8379669f9fedbce46334599a356b
Sam Winkles <swinkles92@live.com>
2023-06-28 14:23:38 -0600

    Completed Weather API Connection
    Had significant issues setting up & using HttpClient in a separate method. Moved all code for the HttpClient instantiation and usage into program's main method.
    
    Program now successfully connects to VisualCrossing's API and will pull today's weather forecast for Albuquerque and display to user.

--- ATM_Manager/Program.Helpers.cs
+++ ATM_Manager/Program.Helpers.cs
@@ -1,9 +1,6 @@
-﻿using SWCBank;
-using Newtonsoft.Json;
-
-partial class Program
+﻿partial class Program
 {
-    public static int takeUserNumericalInput()
+    public static int TakeUserNumericalInput()
     {
         int parsedInput;
         while(!Int32.TryParse(ReadLine(), out parsedInput)) {
@@ -11,16 +8,5 @@ partial class Program
                 "\nPlease try again.");
         }
         return parsedInput;
-    }
-    public async static void connectToWeatherAPI()
-    {
-        var client = new HttpClient();
-
-        var request = new HttpRequestMessage(HttpMethod.Get, " https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Paris?key=<YOUR_API_KEY>");
-
-        var response = await client.SendAsync(request);
-        response.EnsureSuccessStatusCode(); // Throw an exception if error
-
-        var body = await response.ReadAsStringAsync();
     }
 }
--- ATM_Manager/Program.cs
+++ ATM_Manager/Program.cs
@@ -1,7 +1,33 @@
-﻿using SWCBank;
+﻿using Newtonsoft.Json;
+using SWCBank;
 
-ATM test = new ATM(1000);
+// Setup HttpClient for later use
+var client = new HttpClient();
 
-WriteLine("Input");
-//takeUserNumericalInput();
-connectToWeatherAPI();
+// Instantiate ATM object with default starting cash amt & display welcome msg
+ATM atm = new ATM(1000);
+atm.DisplayWelcomeMsg();
+
+// Connect to Visual Crossing's Web API to display today's weather report
+var request = new HttpRequestMessage(HttpMethod.Get,
+    "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/" +
+    "services/timeline/Albuquerque/today?key=BE5A2P7RAMQ7LGKEH8NEZPVTP");
+var response = await client.SendAsync(request);
+response.EnsureSuccessStatusCode();
+var body = await response.Content.ReadAsStringAsync();
+dynamic weather = JsonConvert.DeserializeObject(body);
+
+// The weather object contains an array of day data objects. Since we only
+// requested weather info for today, only one day object exists at pos [0].
+var day = weather.days[0];
+// The following are various data points gathered from the day object.
+// More info on these and more can be found at:
+// https://www.visualcrossing.com/resources/documentation/weather-api/timeline-weather-api/
+string weather_desc = day.description;
+string weather_tmax = day.tempmax;
+string weather_tmin = day.tempmin;
+
+WriteLine("Today's Albuquerque forecast is:");
+WriteLine(" " + weather_desc);
+WriteLine(" With a high of " + weather_tmax + "\u00B0F");
+WriteLine(" and a low of " + weather_tmin + "\u00B0F");
